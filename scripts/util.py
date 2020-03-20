9eaff4136c73e0b6e073136ea3c40c79912b87cb
felipetambasco <tambascofelipe@gmail.com>
2020-03-19 21:12:25 -0300

    Update util script with logos v2 dir path.

--- scripts/util.py
+++ scripts/util.py
@@ -6,5 +6,6 @@ import os
 DATA_DIR = os.path.join('..', 'data')
 
 LOGOS_DIR = os.path.join(DATA_DIR, 'logos')
+LOGOS_V2_DIR = os.path.join(DATA_DIR, 'logos_v2')
 DOCUMENTS_DIR = os.path.join(DATA_DIR, 'documents')
 
