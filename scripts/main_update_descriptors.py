61f07ebe84282b14d31560a65c0d7778674fd16a
felipetambasco <tambascofelipe@gmail.com>
2020-04-11 11:47:21 -0300

    Fix update descriptors file.

--- scripts/main_update_descriptors.py
+++ scripts/main_update_descriptors.py
@@ -23,18 +23,21 @@ logos_features_path = args.descriptors
 
 logos_features = joblib.load(logos_features_path)
 
-temp_descriptors = {}
-logo = cv2.imread(logo_path)
-for f in [sift_features, surf_features, orb_features, brief_features]:
-    keypoints, descriptors = f(logo)
-    keypoints_list = []
-    for point in keypoints:
-        temp = [point.pt, point.size, point.angle, point.response, point.octave, point.class_id]
-        keypoints_list.append(temp)
+if logos_dir is not None:
+    for logo_path_ in os.listdir(logos_dir):
+        logo_path = os.path.join(logos_dir, logo_path_)
+        temp_descriptors = {}
+        logo = cv2.imread(logo_path)
+        for f in [sift_features, surf_features, orb_features, brief_features]:
+            keypoints, descriptors = f(logo)
+            keypoints_list = []
+            for point in keypoints:
+                temp = [point.pt, point.size, point.angle, point.response, point.octave, point.class_id]
+                keypoints_list.append(temp)
 
-    temp_descriptors[f.__name__] = {'keypoints': keypoints_list,
-                                    'descriptors': descriptors}
+            temp_descriptors[f.__name__] = {'keypoints': keypoints_list,
+                                            'descriptors': descriptors}
 
-logos_features[logo_path] = temp_descriptors
+        logos_features[logo_path] = temp_descriptors
 
 joblib.dump(logos_features, logos_features_path)
